49b40f9120329761b29ef13671fc6e31e4d1e95f
philbaker <phil.baker@mumsnet.com>
2020-02-29 20:37:56 +0000

    Add input checkbox

---
+++ src/components/MnInputCheckbox.vue
@@ -0,0 +1,61 @@
+<template>
+    <div
+        :class="wrapperClass"
+    >
+        <input
+            :id="id"
+            :class="inputClass"
+            :name="name"
+            type="checkbox"
+            :value="value"
+            @change="change"
+        >
+
+        <label
+            :class="labelClass"
+            :for="id"
+        >
+            {{ labelText }}
+        </label>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MnInputCheckbox',
+    props: {
+        name: {
+            type: String,
+            required: true
+        },
+        value: {
+            required: true
+        },
+        labelText: {
+            type: String,
+            required: true
+        },
+        id: {
+            type: String,
+            default: ''
+        },
+        wrapperClass: {
+            type: String,
+            default: 'form-check mb-2'
+        },
+        inputClass: {
+            type: String,
+            default: 'form-check-input'
+        },
+        labelClass: {
+            type: String,
+            default: 'form-check-label text-sm'
+        }
+    },
+    methods: {
+        change(e) {
+            this.$emit('change', e);
+        }
+    }
+};
+</script>

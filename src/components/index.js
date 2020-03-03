df9d30316b1da95dba1bcaa11ee03fb0df6dc285
philbaker <phil.baker@mumsnet.com>
2020-03-03 11:29:16 +0000

    Add set email to index

--- src/components/index.js
+++ src/components/index.js
@@ -1,16 +1,17 @@
 import Vue from 'vue';
-import MnAlert from './MnAlert.vue';
-import MnModal from './MnModal.vue';
-import MnActionModal from './MnActionModal.vue';
-import MnContextModal from './MnContextModal.vue';
-import MnLoadSpinner from './MnLoadSpinner.vue';
-import MnTab from './MnTab.vue';
-import MnTabs from './MnTabs.vue';
-import MnInputPassword from './MnInputPassword.vue';
-import MnInput from './MnInput.vue';
-import MnInputCheckbox from './MnInputCheckbox.vue';
-import MnInputRadio from './MnInputRadio.vue';
-import MnContentBlock from './MnContentBlock.vue';
+import MnAlert from './MnAlert';
+import MnModal from './MnModal';
+import MnActionModal from './MnActionModal';
+import MnContextModal from './MnContextModal';
+import MnLoadSpinner from './MnLoadSpinner';
+import MnTab from './MnTab';
+import MnTabs from './MnTabs';
+import MnInputPassword from './MnInputPassword';
+import MnInput from './MnInput';
+import MnInputCheckbox from './MnInputCheckbox';
+import MnInputRadio from './MnInputRadio';
+import MnInputSetEmail from './MnInputSetEmail';
+import MnContentBlock from './MnContentBlock';
 import helpers from '../helpers';
 
 
@@ -26,7 +27,8 @@ const Components = {
     MnInput,
     MnInputCheckbox,
     MnInputRadio,
-    MnContentBlock
+    MnContentBlock,
+    MnInputSetEmail
 };
 
 Object.keys(Components).forEach(name => {
